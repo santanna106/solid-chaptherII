a305fa660f9b47522f01dcf478a6c0d6224fc9c1
Gabriel Andrade <gabriel.santana@gmail.com>
2022-09-14 04:43:57 -0300

    Inclusão do swagger

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,11 @@
+import express from 'express';
 import { app } from ".";
+import swaggerUI from 'swagger-ui-express';
+
+const swaggerFile = require('./swagger.json');
+
+app.use(express.json());
+
+app.use("/api-docs",swaggerUI.serve,swaggerUI.setup(swaggerFile));
 
 app.listen(3333, () => console.log("Server is running!"));
